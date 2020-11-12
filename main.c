ed11a8db491d8ac468cb8c186d48beb955f92361
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-12 16:05:52 +0900

    04

--- main.c
+++ main.c
@@ -2,45 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+char *proverb = "All that glisters is not gold.";
 
-void print_image(int image[][5]);
-void brighten_image(int image[][5]);
+void setPointer(char *q)
+{
+	q = proverb;
+}
 
 int main(void)
 {
-	int image[5][5] = {	{10, 20, 30, 40, 50},
-						{10, 20, 30, 40, 50},
-						{10, 20, 30, 40, 50},
-						{10, 20, 30, 40, 50},
-						{10, 20, 30, 40, 50} };
-	print_image(image);
-	brighten_image(image);
-	print_image(image);
+	char *p = "zzz";
+	setPointer(p);
+	printf("%s\n", p);
 
-	return 0;
+	return 0;	
 }
-
-void print_image(int image[][5]) {
-	int i, j;
-	for (i=0; i<5; i++) {
-		for (j=0; j<5; j++) {
-			printf("%d ", image[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-void brighten_image(int image[][5]) {
-	int i, j;
-	int *ptr;
-	ptr = image;
-	
-	for (i=0; i<5; i++) {
-		for (j=0; j<5; j++) {
-			*ptr = *ptr + 10;
-			ptr++;
-		}	
-	}
-}
-
