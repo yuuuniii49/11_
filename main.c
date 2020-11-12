109147f1e3bb0013ca2c40b4891dd0dc7b837801
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-12 16:00:41 +0900

    05

--- main.c
+++ main.c
@@ -3,17 +3,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+void print_image(int image[][5]);
+void brighten_image(int image[][5]);
 
-	int i = 100;
-	int *p = &i;
-	int **q = &p;
-	
-	*p = 200;  
-	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
-	
-	**q = 300; 
-	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
-	
+int main(void)
+{
+	int image[5][5] = {	{10, 20, 30, 40, 50},
+						{10, 20, 30, 40, 50},
+						{10, 20, 30, 40, 50},
+						{10, 20, 30, 40, 50},
+						{10, 20, 30, 40, 50} };
+	print_image(image);
+	brighten_image(image);
+	print_image(image);
+
 	return 0;
 }
+
+void print_image(int image[][5]) {
+	int i, j;
+	for (i=0; i<5; i++) {
+		for (j=0; j<5; j++) {
+			printf("%d ", image[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void brighten_image(int image[][5]) {
+	int i, j;
+	int *ptr;
+	ptr = image;
+	
+	for (i=0; i<5; i++) {
+		for (j=0; j<5; j++) {
+			*ptr = *ptr + 10;
+			ptr++;
+		}	
+	}
+}
+
